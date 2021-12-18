6e11fabdfd89656bfa009e72187ed9f6ea0c3780
Nearata <nearata@protonmail.com>
2021-12-18 20:46:47 +0100

    pipenv update

--- vvvvid_downloader/__main__.py
+++ vvvvid_downloader/__main__.py
@@ -22,7 +22,8 @@ def main() -> None:
     session = Client(
         headers={
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0"
-        }
+        },
+        http2=True,
     )
 
     # Get connection ID
